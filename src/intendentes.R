4aa29f040b7c3bf5b296a60a6eb2375a77324d95
Getulio Valentin Sánchez <valentin2507@gmail.com>
2021-03-26 23:53:59 -0400

    intendentes: Use dplyr for general performance analysis
    Signed-off-by: Getulio Valentin Sánchez <valentin2507@gmail.com>

--- src/intendentes.R
+++ src/intendentes.R
@@ -8,49 +8,30 @@ library(rstatix)
 #' election of mayors.
 mayor_general_performance <- function(election_results) {
 
-    # Calculates the total number of votes casted in favor of each party in each
-    # election.
-    votes_per_party <- aggregate(votos~anio + siglas_lista,
-                                election_results,
-                                sum)
+  # Calculates the total number of votes casted in favor of each party in each
+  # election.
+  votes_per_party <- election_results %>%
+                       group_by(anio, siglas_lista) %>%
+                       summarise(votos = sum(votos),
+                                .groups = "drop")
 
-    # Sort votes by year (asc) and number of votes (des), so as to make it
-    # easier to get the top parties in each election year.
-    #' votes_per_party <- votes_per_party[order(votes_per_party$anio,
-    #'                                        -votes_per_party$votos), ]
+  # Calculates the total number of votes casted in each election.
+  total_votes <- election_results %>%
+                   group_by(anio, disdes, zon, loc) %>%
+                   # Total votes per district are repeated, get the first one.
+                   slice_head() %>%
+                   group_by(anio) %>%
+                   summarise(total_votos = sum(total_votos))
 
-    # Fitlers the top 2 parties for each election year.
-    # ' most_voted_parties <- by(votes_per_party,
-    #'                         votes_per_party["anio"],
-    #'                         head,
-    #'                         n = 2)
-    #' most_voted_parties <- Reduce(rbind, most_voted_parties)
+  # Calculates the ANR general performance over the years.
+  anr_votes <- votes_per_party %>%
+                 filter(siglas_lista == "ANR") %>%
+                 mutate(total_votos = total_votes$total_votos,
+                        vote_share = anr_votes$votos / total_votes$total_votos)
 
-    #' print(head(most_voted_parties))
-    #' print(nrow(most_voted_parties))
-
-    # The 1996 election didn't separate vote count for each zone and location.
-    # The data was about an entire district, so the dataset has NA values for
-    # loc and zon.
-    #
-    # If these values are not set, results for 1996 are not included in
-    # aggregation. Set them to 0 for analysis purposes.
-    election_results[is.na(election_results$loc), ]$loc <- 0
-    election_results[is.na(election_results$zon), ]$zon <- 0
-
-
-    # Calculates the total number of votes casted in each election.
-    total_votes <- aggregate(total_votos~anio + disdes + zon + loc,
-                                    election_results, head, 1)
-    total_votes <- aggregate(total_votos~anio, total_votes, sum)
-
-    # Calculates the ANR general performance over the years.
-    anr_votes <- votes_per_party[votes_per_party$siglas_lista == "ANR", ]
-    anr_votes$total_votos <- total_votes$total_votos
-    anr_votes$vote_share <- anr_votes$votos / total_votes$total_votos
-
-    print(anr_votes)
+  print(anr_votes)
 }
+
 
 #' Runs yearly pairwise comparisons of the mean of vote shares per district.
 #'
@@ -58,11 +39,11 @@ mayor_general_performance <- function(election_results) {
 #' election of mayors.
 mayor_pwc_vote_share_per_year <- function(election_results) {
   votes_per_party <- election_results %>%
-                      group_by(anio, disdes, siglas_lista) %>%
-                      summarise(votos = sum(votos),
-                                total_votos = sum(total_votos),
-                                .groups = "drop") %>%
-                      mutate(vote_share = votos / total_votos)
+                       group_by(anio, disdes, siglas_lista) %>%
+                       summarise(votos = sum(votos),
+                                 total_votos = sum(total_votos),
+                                 .groups = "drop") %>%
+                       mutate(vote_share = votos / total_votos)
 
   anr_results <- votes_per_party %>%
                    filter(siglas_lista == "ANR", !is.na(vote_share))
