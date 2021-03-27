caeaa980f1a0e5d580c2b8b324614ab01dee9ffe
Getulio Valentin Sánchez <valentin2507@gmail.com>
2021-03-27 18:21:48 -0400

    intendentes: Measure performance per district over years
    Signed-off-by: Getulio Valentin Sánchez <valentin2507@gmail.com>

--- src/elecciones.R
+++ src/elecciones.R
@@ -2,13 +2,13 @@ setwd("~/Data/Elecciones/eleccionespy")
 
 source("./src/intendentes.R")
 
-# Import and clean results.
-results <- read.csv(file = "./data/resultados-1996-2018.csv")
-results <- subset(results, select = -c(depdes, zondes, locdes,
+# Import and clean election results.
+election_results <- read.csv(file = "./data/resultados-1996-2018.csv")
+election_results <- subset(results, select = -c(zondes, locdes,
                                       candidatura, nombre_lista, lista))
 colnames(results)[1] <- "anio"
 
 mayor_results <- results[results$cand_desc == "INTENDENTE", ]
-
 mayor_general_performance(mayor_results)
-mayor_pwc_vote_share_per_year(mayor_results)
+mayor_pwc_voteshare_per_year(mayor_results)
+mayor_delta_voteshare_per_city(mayor_results)
--- src/intendentes.R
+++ src/intendentes.R
@@ -27,7 +27,7 @@ mayor_general_performance <- function(election_results) {
   anr_votes <- votes_per_party %>%
                  filter(siglas_lista == "ANR") %>%
                  mutate(total_votos = total_votes$total_votos,
-                        vote_share = anr_votes$votos / total_votes$total_votos)
+                        vote_share = votos / total_votes$total_votos)
 
   print(anr_votes)
 }
@@ -37,7 +37,7 @@ mayor_general_performance <- function(election_results) {
 #'
 #' This functions expects that the results dataframe only corresponds to the
 #' election of mayors.
-mayor_pwc_vote_share_per_year <- function(election_results) {
+mayor_pwc_voteshare_per_year <- function(election_results) {
   votes_per_party <- election_results %>%
                        group_by(anio, disdes, siglas_lista) %>%
                        summarise(votos = sum(votos),
@@ -53,4 +53,55 @@ mayor_pwc_vote_share_per_year <- function(election_results) {
            tukey_hsd(vote_share ~ as.factor(anio))
 
   print(pwc)
+}
+
+mayor_delta_voteshare_per_city <- function(election_results) {
+  # Obtains election results per districct and year for ANR and calculates the
+  # vote share.
+  anr_results <- election_results %>%
+                   filter(siglas_lista == "ANR") %>%
+                   group_by(anio, depdes, disdes) %>%
+                   summarise(votos = sum(votos),
+                             total_votos = sum(total_votos),
+                             .groups = "drop") %>%
+                   mutate(vote_share = votos / total_votos * 100)
+
+  # Group the results by district in order to add a change in vote share between
+  # 2 consecutive years.
+  #
+  # Arranging by disdes is needed since lag only works using the previous row,
+  # and we want to take the difference in vote share between 2 consecutive years
+  # for the same district.
+  #
+  # The row with the base case (first election for a district) gets a NA for
+  # delta
+  anr_results <- anr_results %>%
+                   arrange(depdes, disdes) %>%
+                   group_by(depdes, disdes) %>%
+                   filter(n() > 2) %>%
+                   mutate(delta = lag(vote_share) - vote_share)
+
+  # Save the change in vote share per year and district.
+  anr_results %>%
+    select(depdes, disdes, delta) %>%
+    write.csv(file = "./data/output/anr_delta_voteshare_district.csv")
+
+  # Runs a simple model in which, for each district, we take the average change
+  # in vote share over the years.
+  anr_results <- anr_results %>%
+                   filter(!is.na(delta))  %>%
+                   group_by(depdes, disdes) %>%
+                   summarise(avg_delta = mean(delta))
+
+  # Save the average change in vote shater per year and district.
+  anr_results %>%
+    select(depdes, disdes, avg_delta) %>%
+    write.csv(file = "./data/output/anr_avg_delta_voteshare.csv")
+
+  anr_results <- anr_results %>%
+                   group_by(depdes) %>%
+                   summarise(avg_of_avg_delta = mean(avg_delta)) %>%
+                   arrange(desc(avg_of_avg_delta))
+
+  print(anr_results)
 }
